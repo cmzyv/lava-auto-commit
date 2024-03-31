297cd1c0d629490ad18008135eaa64d20e19ae66
Comzvy <44536997+cmzyv@users.noreply.github.com>
2024-03-31 10:40:15 +0700

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-507aa5b2-e658-4470-82c9-c8545e9e43a0/"
 
 OUTPUT_DIR="logs"
 
